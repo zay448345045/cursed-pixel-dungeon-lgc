11f75a20341bddedd0547c08b3ec1189941690e4
samue <smujamesb@gmail.com>
2019-07-30 10:50:43 +0100

    Updated depth generation

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/GameScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/GameScene.java
@@ -351,22 +351,28 @@ public class GameScene extends PixelScene {
 			break;
 		case DESCEND:
 			switch (Dungeon.depth) {
-			case 1:
-				WndStory.showChapter( WndStory.ID_SEWERS );
-				break;
-			case 6:
-				WndStory.showChapter( WndStory.ID_PRISON );
-				break;
-			case 11:
-				WndStory.showChapter( WndStory.ID_CAVES );
-				break;
-			case 16:
-				WndStory.showChapter( WndStory.ID_CITY );
-				break;
-			case 22:
-				WndStory.showChapter( WndStory.ID_HALLS );
-				break;
-			}
+				case 1:
+					WndStory.showChapter(WndStory.ID_SEWERS);
+					break;
+				case 6:
+					WndStory.showChapter(WndStory.ID_PRISON);
+					break;
+				case 11:
+					WndStory.showChapter(WndStory.ID_CAVES);
+					break;
+				case 16:
+					WndStory.showChapter(WndStory.ID_CITY);
+					break;
+				case 22:
+					WndStory.showChapter(WndStory.ID_HALLS);
+					break;
+
+				case 31:
+					WndStory.showChapter(WndStory.ID_HALLS);
+					break;
+
+
+		}
 			if (Dungeon.hero.isAlive() && Dungeon.depth != 22) {
 				Badges.validateNoKilling();
 			}
