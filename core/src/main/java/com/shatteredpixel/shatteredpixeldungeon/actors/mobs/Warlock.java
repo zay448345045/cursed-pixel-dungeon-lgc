751436ae075f036a105093a6dd6d755f10be8161
samue <smujamesb@gmail.com>
2019-07-30 14:28:03 +0100

    Fixed formatting errors

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Warlock.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Warlock.java
@@ -106,7 +106,7 @@ public class Warlock extends Mob implements Callback {
 				Buff.prolong( enemy, Weakness.class, Weakness.DURATION );
 			}
 			
-			int dmg = Random.Int( 12, 18 );
+			int dmg = Random.Int( 30, 40 );
 			enemy.damage( dmg, new DarkBolt() );
 			
 			if (!enemy.isAlive() && enemy == Dungeon.hero) {
