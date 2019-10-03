d68a40863375b766c2016acd0fd7ed09ba0d5b16
samue <smujamesb@gmail.com>
2019-10-03 15:53:20 +0100

    Earth Challenge fixed

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/tiles/TerrainFeaturesTilemap.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/tiles/TerrainFeaturesTilemap.java
@@ -64,7 +64,7 @@ public class TerrainFeaturesTilemap extends DungeonTilemap {
 			return plants.get(pos).image + 7*16;
 		}
 
-		int stage = (Dungeon.depth-1)/5;
+		int stage = Math.min((Dungeon.depth-1)/5,5);
 		if (Dungeon.depth == 21) stage--;
 		if (tile == Terrain.HIGH_GRASS){
 			return 9 + 16*stage + (DungeonTileSheet.tileVariance[pos] >= 50 ? 1 : 0);
