c2885f22bf2e02ed4d71175fd1ee2636c327e8fd
samue <smujamesb@gmail.com>
2019-10-04 18:33:01 +0100

    Fixed excess gas from excess damage.

--- core/src/main/java/com/shatteredpixel/cursedpixeldungeon/actors/mobs/Mob.java
+++ core/src/main/java/com/shatteredpixel/cursedpixeldungeon/actors/mobs/Mob.java
@@ -584,7 +584,8 @@ public abstract class Mob extends Char {
 			alerted = true;
 		}
 		if (Dungeon.isChallenged(Challenges.EVIL_GAS)) {
-			GameScene.add(Blob.seed(this.pos, (int) (50*((float)dmg/(float)HT)), EvilGas.class).setStrength(1+Dungeon.depth/4));
+			int amount = Math.min( (int) (50*((float)dmg/(float)HT)), 1);
+			GameScene.add(Blob.seed(this.pos, amount, EvilGas.class).setStrength(1+Dungeon.depth/4));
 		}
 		super.damage( dmg, src );
 	}
