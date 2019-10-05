529941e10a6ecb61dfa90a70652728d64e4a3d9b
samue <smujamesb@gmail.com>
2019-10-05 11:22:24 +0100

    Fossil Skeleton added

---
+++ core/src/main/java/com/shatteredpixel/cursedpixeldungeon/actors/mobs/FossilSkeleton.java
@@ -0,0 +1,88 @@
+package com.shatteredpixel.cursedpixeldungeon.actors.mobs;
+
+import com.shatteredpixel.cursedpixeldungeon.Assets;
+import com.shatteredpixel.cursedpixeldungeon.Dungeon;
+import com.shatteredpixel.cursedpixeldungeon.actors.Char;
+import com.shatteredpixel.cursedpixeldungeon.items.Generator;
+import com.shatteredpixel.cursedpixeldungeon.items.Gold;
+import com.shatteredpixel.cursedpixeldungeon.items.Item;
+import com.shatteredpixel.cursedpixeldungeon.levels.features.Chasm;
+import com.shatteredpixel.cursedpixeldungeon.messages.Messages;
+import com.shatteredpixel.cursedpixeldungeon.sprites.SkeletonSprite;
+import com.shatteredpixel.cursedpixeldungeon.utils.GLog;
+import com.watabou.noosa.audio.Sample;
+import com.watabou.utils.PathFinder;
+import com.watabou.utils.Random;
+
+public class FossilSkeleton extends Mob {
+
+    {
+        spriteClass = SkeletonSprite.class;
+
+        HP = HT = 200;
+        defenseSkill = 20;
+
+        EXP = 20;
+        maxLvl = 30;
+
+        loot = Generator.Category.WEAPON;
+        lootChance = 0.125f;
+
+        properties.add(Property.UNDEAD);
+        properties.add(Property.INORGANIC);
+    }
+
+    @Override
+    public int damageRoll() {
+        return Random.NormalIntRange(50, 110);
+    }
+
+    @Override
+    public void die(Object cause) {
+
+        super.die(cause);
+
+        if (cause == Chasm.class) return;
+
+        boolean heroKilled = false;
+        for (int i = 0; i < PathFinder.NEIGHBOURS8.length; i++) {
+            Char ch = findChar(pos + PathFinder.NEIGHBOURS8[i]);
+            if (ch != null && ch.isAlive()) {
+                int damage = Random.NormalIntRange(60, 120);
+                damage = Math.max(0, damage - (ch.drRoll() + ch.drRoll()));
+                ch.damage(damage, this);
+                if (ch == Dungeon.hero && !ch.isAlive()) {
+                    heroKilled = true;
+                }
+            }
+        }
+
+        if (Dungeon.level.heroFOV[pos]) {
+            Sample.INSTANCE.play(Assets.SND_BONES);
+        }
+
+        if (heroKilled) {
+            Dungeon.fail(getClass());
+            GLog.n(Messages.get(this, "explo_kill"));
+        }
+    }
+
+    @Override
+    protected Item createLoot() {
+        Item result = new Gold().random();
+        result.quantity(Math.round(result.quantity() * Random.NormalFloat(0.33f, 1f)));
+        return result;
+    }
+
+    @Override
+    public int attackSkill(Char target) {
+        return 40;
+    }
+
+    @Override
+    public int drRoll() {
+        return Random.NormalIntRange(0, 5);
+    }
+
+}
+
