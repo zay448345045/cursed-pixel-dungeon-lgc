d9052908703cab4f5e842b9027844b3ce66a0558
samue <smujamesb@gmail.com>
2019-10-07 14:18:11 +0100

    Wand of Warding added to generator and removed from starting gear

--- core/src/main/java/com/shatteredpixel/cursedpixeldungeon/items/wands/WandOfCorruption.java
+++ core/src/main/java/com/shatteredpixel/cursedpixeldungeon/items/wands/WandOfCorruption.java
@@ -129,7 +129,7 @@ public class WandOfCorruption extends Wand {
 
 			Mob enemy = (Mob) ch;
 
-			float corruptingPower = 2 + level();
+			float corruptingPower = 1+  (level()/2f);
 			
 			//base enemy resistance is usually based on their exp, but in special cases it is based on other criteria
 			float enemyResist = 1 + enemy.EXP;
