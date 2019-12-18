1c5f5b29c5fbff9f17814eb4ff439560a770ff88
samue <smujamesb@gmail.com>
2019-12-18 14:05:57 +0000

    Chainsaw working properly

--- core/src/main/java/com/shatteredpixel/cursedpixeldungeon/items/weapon/melee/RelicMeleeWeapons/RelicEnchantments/Bloodlust.java
+++ core/src/main/java/com/shatteredpixel/cursedpixeldungeon/items/weapon/melee/RelicMeleeWeapons/RelicEnchantments/Bloodlust.java
@@ -2,9 +2,14 @@ package com.shatteredpixel.cursedpixeldungeon.items.weapon.melee.RelicMeleeWeapo
 
 import com.shatteredpixel.cursedpixeldungeon.actors.Char;
 import com.shatteredpixel.cursedpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.cursedpixeldungeon.items.weapon.melee.RelicMeleeWeapons.ChainsawHand;
 import com.shatteredpixel.cursedpixeldungeon.items.weapon.melee.RelicMeleeWeapons.RelicMeleeWeapon;
+import com.shatteredpixel.cursedpixeldungeon.messages.Messages;
 import com.shatteredpixel.cursedpixeldungeon.sprites.ItemSprite;
+import com.shatteredpixel.cursedpixeldungeon.utils.GLog;
 import com.watabou.utils.Callback;
+
+import javax.microedition.khronos.opengles.GL;
 
 public class Bloodlust extends RelicEnchantment {
 
@@ -23,15 +28,19 @@ public class Bloodlust extends RelicEnchantment {
     @Override
     public int relicProc(RelicMeleeWeapon weapon, Char attacker, final Char defender, final int damage) {
         if (attacker instanceof Hero & defender.isAlive()) {
-            weapon.use(1);
-            final Hero hero = (Hero) attacker;
-            hero.sprite.attack(defender.pos, new Callback() {
-                @Override
-                public void call() {
-                    hero.actuallyAttack(defender);
-                    hero.next();
-                }
-            });
+            if (weapon.charge > 0 & ((ChainsawHand)weapon).isTurnedOn()) {
+                weapon.use(2);
+                final Hero hero = (Hero) attacker;
+                hero.sprite.attack(defender.pos, new Callback() {
+                    @Override
+                    public void call() {
+                        hero.actuallyAttack(defender);
+                        hero.next();
+                    }
+                });
+            } else {
+                GLog.n(Messages.get(Bloodlust.class,"no_charge"));
+            }
         } else {
             attacker.next();
             return damage;//TODO: allow proccing from Statues and Ghost
