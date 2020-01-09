a07271907db5f8ba093ac967dc71a7ba7bdfa5d3
samue <smujamesb@gmail.com>
2020-01-09 11:33:25 +0000

    Added deferred death and miasma

---
+++ core/src/main/java/com/shatteredpixel/cursedpixeldungeon/actors/buffs/DeferredDeath.java
@@ -0,0 +1,59 @@
+package com.shatteredpixel.cursedpixeldungeon.actors.buffs;
+
+import com.shatteredpixel.cursedpixeldungeon.actors.blobs.Blob;
+import com.shatteredpixel.cursedpixeldungeon.actors.blobs.Miasma;
+import com.shatteredpixel.cursedpixeldungeon.effects.CellEmitter;
+import com.shatteredpixel.cursedpixeldungeon.effects.particles.ShadowParticle;
+import com.shatteredpixel.cursedpixeldungeon.items.weapon.enchantments.Grim;
+import com.shatteredpixel.cursedpixeldungeon.messages.Messages;
+import com.shatteredpixel.cursedpixeldungeon.scenes.GameScene;
+import com.shatteredpixel.cursedpixeldungeon.sprites.CharSprite;
+import com.shatteredpixel.cursedpixeldungeon.ui.BuffIndicator;
+
+public class DeferredDeath extends FlavourBuff {
+
+    {
+        type = buffType.NEGATIVE;
+        announced = true;
+    }
+
+    @Override
+    public void fx(boolean on) {
+        if (on) target.sprite.add(CharSprite.State.DARKENED);
+        else if (target.invisible == 0) target.sprite.remove(CharSprite.State.DARKENED);
+    }
+
+    @Override
+    public int icon() {
+        return BuffIndicator.CORRUPT;
+    }
+
+    @Override
+    public String toString() {
+        return Messages.get(this, "name");
+    }
+
+    @Override
+    public String desc() {
+        return Messages.get(this, "desc", dispTurns());
+    }
+
+    @Override
+    public boolean act() {
+        return super.act();
+    }
+
+    public void recover(float amount) {
+        spend(-amount);
+        if (cooldown() <= 0){
+            detach();
+        }
+    }
+    @Override
+    public void detach() {
+        super.detach();
+        target.damage(target.HP, new Grim());
+        GameScene.add(Blob.seed(target.pos, 100, Miasma.class));
+        CellEmitter.get(target.pos).burst(ShadowParticle.UP, 20);
+    }
+}
--- core/src/main/java/com/shatteredpixel/cursedpixeldungeon/items/Soul.java
+++ core/src/main/java/com/shatteredpixel/cursedpixeldungeon/items/Soul.java
@@ -4,9 +4,11 @@ import com.shatteredpixel.cursedpixeldungeon.actors.buffs.Adrenaline;
 import com.shatteredpixel.cursedpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.cursedpixeldungeon.actors.buffs.Weakness;
 import com.shatteredpixel.cursedpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.cursedpixeldungeon.actors.mobs.Mob;
 import com.shatteredpixel.cursedpixeldungeon.effects.Speck;
 import com.shatteredpixel.cursedpixeldungeon.items.rings.RingOfLuck;
 import com.shatteredpixel.cursedpixeldungeon.sprites.ItemSpriteSheet;
+import com.watabou.utils.Bundle;
 
 import java.util.ArrayList;
 
@@ -17,9 +19,12 @@ public class Soul extends Item {
         unique = true;
         cursed = false;
     }
+    private Mob mob = null;
+
     private static final String AC_CONSUME = "consume";
     private static final String AC_SACRIFICE = "sacrifice";
     private static final String AC_SUMMON = "summon";
+    private static final String MOB = "mob";
 
     @Override
     public ArrayList<String> actions(Hero hero) {
@@ -28,6 +33,18 @@ public class Soul extends Item {
         actions.add(AC_SACRIFICE);
         actions.add(AC_SUMMON);
         return actions;
+    }
+
+    @Override
+    public void storeInBundle(Bundle bundle) {
+        bundle.put(MOB, mob);
+        super.storeInBundle(bundle);
+    }
+
+    @Override
+    public void restoreFromBundle(Bundle bundle) {
+        mob = (Mob) bundle.get(MOB);
+        super.restoreFromBundle(bundle);
     }
 
     private void ACConsume(Hero hero) {
@@ -44,7 +61,11 @@ public class Soul extends Item {
         hero.damage(hero.HT/20, this);
         Buff.prolong(hero, Adrenaline.class, 1 + RingOfLuck.randomInt((int)Adrenaline.DURATION-1, (int)Adrenaline.DURATION));
     }
-    private void ACSummon(Hero hero) {}
+    private void ACSummon(Hero hero) {
+        if (mob != null) {
+
+        }
+    }
 
     @Override
     public boolean isIdentified() {
