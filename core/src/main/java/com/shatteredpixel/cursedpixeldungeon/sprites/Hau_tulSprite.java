971a30a5fdb987083444f5f2fa243c9be8e932fc
samue <smujamesb@gmail.com>
2020-01-31 18:23:26 +0000

    Sprite of new NPC fixed

--- core/src/main/java/com/shatteredpixel/cursedpixeldungeon/sprites/Hau_tulSprite.java
+++ core/src/main/java/com/shatteredpixel/cursedpixeldungeon/sprites/Hau_tulSprite.java
@@ -9,19 +9,12 @@ public class Hau_tulSprite extends MobSprite {
 
         texture( Assets.HUMAN );
 
-        TextureFilm frames = new TextureFilm( texture, 16, 15 );
+        TextureFilm frames = new TextureFilm( texture, 14, 15 );
 
-        idle = new Animation( 2, true );
-        idle.frames( frames, 16, 16, 16, 17 );
+        idle = new Animation( 4, true );
+        idle.frames( frames, 18, 18, 18, 19 );
 
-        run = new Animation( 10, true );
-        run.frames( frames, 22, 23, 24, 25, 26 );
-
-        attack = new Animation( 15, false );
-        attack.frames( frames, 18, 19, 20, 21 );
-
-        die = new Animation( 10, false );
-        die.frames( frames, 27, 28, 29, 30 );
+        die = idle.clone();
 
         play( idle );
     }
